13ec198695ff3be940ada566a1430d3704a16dea
fault <critical@fault.io>
2022-07-19 14:29:34 -0700

    Remove unused execution tracing and add support for metrics telemetry with LLVM instrumentation.

--- machines/include/src/fault/metrics.h
+++ machines/include/src/fault/metrics.h
@@ -1,134 +1,60 @@
 /**
-	# Support for directing measurement collections for instrumented processes.
+	// Control telemetry locations for supported instrumentation frameworks.
 */
-#include <stdlib.h>
-#include <fcntl.h>
-#include <unistd.h>
-#include <stdio.h>
-#include <signal.h>
-#include <errno.h>
-#include <string.h>
 
-#include <sys/types.h>
-#include <sys/stat.h>
+/**
+	// fault-metrics LLVM profile support.
+*/
+#if defined(F_LLVM_INSTRUMENTATION) && defined(F_TELEMETRY_metrics)
+	#include <fault/fs.h>
+	void __llvm_profile_write_file(void);
+	void __llvm_profile_reset_counters(void);
+	void __llvm_profile_set_filename(const char *);
+	void __llvm_profile_initialize_file(void);
 
-#ifndef FAULT_MAIN
-	#define FAULT_MAIN _fault_application_main
-#endif
+	/**
+		// Assign the target profdata file.
+		// Performed atexit, registered by _fault_llvm_telemetry_register.
+	*/
+	static void __attribute__((destructor))
+	_fault_llvm_telemetry_dispatch(void)
+	{
+		static char ifbuf[4096];
+		char pibuf[32];
+		const char *mcp = getenv("METRICS_CAPTURE");
+		const char *pid = getenv("PROCESS_IDENTITY");
+		const char *mid = getenv("METRICS_IDENTITY");
 
-#ifndef EXECUTABLE_NAME
-	#ifdef FACTOR_BASENAME_STR
-		#define EXECUTABLE_NAME FACTOR_BASENAME_STR
-	#else
-		#define EXECUTABLE_NAME "unspecified"
+		/* METRICS_CAPTURE or the compile time default. */
+		if (mcp == NULL)
+			mcp = F_TELEMETRY_metrics;
+
+		/* PROCESS_IDENTITY or the string representation of getpid() */
+		if (pid == NULL)
+		{
+			pid = pibuf;
+			snprintf(pibuf, sizeof(pibuf), "%ld", (long) getpid());
+		}
+
+		/* METRICS_IDENTITY or the constant. */
+		if (mid == NULL)
+			mid = ".fault-llvm";
+
+		snprintf(ifbuf, sizeof(ifbuf),
+			"%s/%s/%s/.llvm.profraw",
+			mcp, pid, mid
+		);
+
+		fs_alloc(0, ifbuf, S_IRWXU|S_IRWXG|S_IRWXO);
+		__llvm_profile_set_filename(ifbuf);
+	}
+
+	#if 0
+	static void __attribute__((constructor))
+	_fault_llvm_telemetry_register(void)
+	{
+		/* Defer file assignment in case of environment changes. */
+		atexit(_fault_llvm_telemetry_dispatch);
+	}
 	#endif
 #endif
-
-static char _fault_execution_record[4096];
-
-/**
-	# Record the execution of the process signaling.
-*/
-static void
-_fault_note_execution_enter(const char *program_name, const char *type)
-{
-	int fd = -1;
-	fd = open(_fault_execution_record, O_WRONLY|O_CREAT);
-
-	if (fd == -1)
-	{
-		fprintf(stderr,
-			"[!* FATAL: `%s` could not be opened for writing]\n", _fault_execution_record);
-		fprintf(stderr,
-			"\tSystem call 'open' failed(%d): %s.\n", errno, strerror(errno));
-		kill(getpid(), SIGUSR2);
-		exit(1);
-	}
-
-	fchmod(fd, S_IRUSR|S_IWUSR);
-
-	dprintf(fd,
-		"name: %s\n"
-		"type: %s\n"
-		"pid: %d\n"
-		"factor: %s\n"
-		"",
-		program_name,
-		type,
-		(int) getpid(),
-		FACTOR_QNAME_STR
-	);
-
-	close(fd);
-}
-
-/**
-	# Record the exit status of the process and other resource usage information.
-	# A measurement snapshot will not have direct access to the status of an exiting
-	# process, so the record is noted in this file for subsequent processing.
-*/
-static void FAULT_MCA
-_fault_note_execution_exit(int status)
-{
-	int fd = -1;
-	fd = open(_fault_execution_record, O_WRONLY|O_APPEND);
-
-	dprintf(fd, "status: %d\n", status);
-
-	close(fd);
-}
-
-/**
-	# Check the (system/envvar)`FAULT_MEASUREMENT_CONTEXT`.
-	# Validate that it is empty and can be stat'd.
-*/
-static void
-_fault_validate_context(const char *context_path)
-{
-	struct stat ctxdir = {0,};
-	int r = -1;
-
-	if (context_path == NULL || context_path[0] == '\0')
-	{
-		fprintf(stderr, "[!* FATAL: FAULT_CAPTURE_DIRECTORY not specified]\n");
-		goto fail;
-	}
-
-	r = stat(context_path, &ctxdir);
-	if (r != 0)
-	{
-		fprintf(stderr, "[!* FATAL: FAULT_CAPTURE_DIRECTORY is not accessible]\n");
-		fprintf(stderr, "\tSystem call 'stat' failed(%d): %s.\n", errno, strerror(errno));
-		goto fail;
-	}
-
-	return;
-
-	fail:
-	{
-		kill(getpid(), SIGUSR2);
-		exit(1);
-	}
-}
-
-/**
-	# Call the program's real main noting its execution.
-*/
-int
-main(int argc, const char *argv[])
-{
-	int result;
-	char *s;
-	int pid = (int) getpid();
-
-	s = getenv("FAULT_CAPTURE_DIRECTORY");
-	_fault_validate_context(s);
-
-	snprintf(_fault_execution_record, sizeof(_fault_execution_record)-1, "%s/process-executed.%d", s, pid);
-
-	FAULT_EXECUTABLE_ENTER(EXECUTABLE_NAME, "root");
-	result = FAULT_MAIN(argc, argv);
-	FAULT_EXECUTABLE_EXIT(result);
-
-	return result;
-}
