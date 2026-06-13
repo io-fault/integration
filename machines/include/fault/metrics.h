3e1d394f9f59221b994996a83bb74c6bc486b11d
fault <critical@fault.io>
2026-06-13 16:14:49 -0700

    Implement the metrics link stack so that instrumented elements factors may associate the telemetry of another factor.

--- machines/include/fault/metrics.h
+++ machines/include/fault/metrics.h
@@ -1,5 +1,11 @@
 /**
 	// Control telemetry locations for supported instrumentation frameworks.
+
+	// [ Integration Control Defines ]
+	// /FAULT_METRICS_LINKED/
+		// Define used to signal that the telemetry will be written by another
+		// factor and that only a link should be created when the captured
+		// data is transmitted.
 */
 
 /**
@@ -23,10 +29,24 @@
 	void __llvm_profile_reset_counters(void);
 	void __llvm_profile_set_filename(const char *);
 	void __llvm_profile_initialize_file(void);
-	static char _fault_llvm_imd[4096 * 2];
-	static char _fault_llvm_imr[4096 * 2];
-	static char _fault_lcounters[4096 * 2];
+
+	// Link stack for creating links to the captured data.
+	struct _fault_metrics_chain {
+		void (*update)(void);
+		struct _fault_metrics_chain *next;
+	};
+
 	static char _fault_metrics_isolation[512];
+	static char _fault_lcounters[4096 * 2];
+	#ifndef FAULT_METRICS_LINKED
+		static char _fault_llvm_imd[4096 * 2];
+		static char _fault_llvm_imr[4096 * 2];
+		char *_fault_metrics_link = _fault_lcounters;
+		struct _fault_metrics_chain *_fault_metrics_link_stack = NULL;
+	#else
+		extern char *_fault_metrics_link;
+		extern struct _fault_metrics_chain *_fault_metrics_link_stack;
+	#endif
 
 	/**
 		// Assign the target profdata file.
@@ -82,24 +102,26 @@
 		snprintf(_fault_metrics_isolation, sizeof(_fault_metrics_isolation),
 			"%s", mi
 		);
-		snprintf(_fault_llvm_imr, sizeof(_fault_llvm_imr),
-			"%s/%s/%s/.fault-llvm-counters/%s.raw",
-			mcp, pid, mid, mi
-		);
-		snprintf(_fault_llvm_imd, sizeof(_fault_llvm_imd),
-			"%s/%s/%s/.fault-llvm-counters/%s.merged",
-			mcp, pid, mid, mi
-		);
 		snprintf(_fault_lcounters, sizeof(_fault_lcounters),
 			"%s/%s/%s/.fault-syntax-counters/l-counters",
 			mcp, pid, mid
 		);
 
 		// Allocate directories for l-counters and LLVM data files.
-		fs_alloc(0, _fault_lcounters, S_IRWXU|S_IRWXG|S_IRWXO);
-		fs_alloc(0, _fault_llvm_imr, S_IRWXU|S_IRWXG|S_IRWXO);
+		fs_alloc(fs_mkdir_defaults, _fault_lcounters, S_IRWXU|S_IRWXG|S_IRWXO);
 
-		__llvm_profile_set_filename(_fault_llvm_imr);
+		#ifndef FAULT_METRICS_LINKED
+			snprintf(_fault_llvm_imr, sizeof(_fault_llvm_imr),
+				"%s/%s/%s/.fault-llvm-counters/%s.raw",
+				mcp, pid, mid, mi
+			);
+			snprintf(_fault_llvm_imd, sizeof(_fault_llvm_imd),
+				"%s/%s/%s/.fault-llvm-counters/%s.merged",
+				mcp, pid, mid, mi
+			);
+			fs_alloc(fs_mkdir_defaults, _fault_llvm_imr, S_IRWXU|S_IRWXG|S_IRWXO);
+			__llvm_profile_set_filename(_fault_llvm_imr);
+		#endif
 		#undef _f_empty_string
 	}
 
@@ -110,60 +132,77 @@
 		pthread_atfork(NULL, NULL, _fault_update_telemetry);
 	}
 
-	static void __attribute__((destructor))
-	_fault_llvm_telemetry_convert(void)
-	{
-		pid_t pid = 0;
-		int fd = -1, status = -1;
-		posix_spawn_file_actions_t fa;
-
-		char *lpd_argv[] = {
-			CC_PATH "/.llvm/pd-tool", "merge",
-			"-o", _fault_llvm_imd,
-			_fault_llvm_imr, NULL
-		};
-		char *imq_argv[] = {
-			CC_PATH "/.llvm/clang-ipq", "counters",
-			F_FACTOR_IMAGE,
-			_fault_llvm_imd, NULL
-		};
-
-		// No llvm-profdata executable.
-		if (access(lpd_argv[0], X_OK) != 0)
-			return;
-
-		// No ipquery executable.
-		if (access(imq_argv[0], X_OK) != 0)
-			return;
-
-		// Try writing. (earlier version of LLVM presumably)
-		if (access(_fault_llvm_imr, F_OK) != 0)
-			__llvm_profile_write_file();
-
-		if (access(_fault_llvm_imr, F_OK) != 0)
+	#ifndef FAULT_METRICS_LINKED
+		static void __attribute__((destructor))
+		_fault_llvm_telemetry_convert(void)
 		{
-			// No data to process.
-			return;
+			pid_t pid = 0;
+			int fd = -1, status = -1;
+			posix_spawn_file_actions_t fa;
+
+			char *lpd_argv[] = {
+				CC_PATH "/.llvm/pd-tool", "merge",
+				"-o", _fault_llvm_imd,
+				_fault_llvm_imr, NULL
+			};
+			char *imq_argv[] = {
+				CC_PATH "/.llvm/clang-ipq", "counters",
+				F_FACTOR_IMAGE,
+				_fault_llvm_imd, NULL
+			};
+
+			// No llvm-profdata executable.
+			if (access(lpd_argv[0], X_OK) != 0)
+				return;
+
+			// No ipquery executable.
+			if (access(imq_argv[0], X_OK) != 0)
+				return;
+
+			// Try writing. (earlier version of LLVM presumably)
+			if (access(_fault_llvm_imr, F_OK) != 0)
+				__llvm_profile_write_file();
+
+			if (access(_fault_llvm_imr, F_OK) != 0)
+			{
+				// No data to process.
+				return;
+			}
+
+			// Merge the raw data so that it can be read.
+			posix_spawn_file_actions_init(&fa);
+			posix_spawn_file_actions_adddup2(&fa, STDERR_FILENO, STDOUT_FILENO);
+			posix_spawn(&pid, lpd_argv[0], &fa, NULL, lpd_argv, NULL);
+			posix_spawn_file_actions_destroy(&fa);
+			waitpid(pid, &status, 0);
+
+			// Prepare l-counters, and switch the metrics isolation.
+			fd = open(_fault_lcounters, O_WRONLY|O_CREAT|O_APPEND, S_IRUSR|S_IWUSR);
+
+			// Extract counters from the merged data.
+			posix_spawn_file_actions_init(&fa);
+			posix_spawn_file_actions_adddup2(&fa, fd, STDOUT_FILENO);
+			posix_spawn(&pid, imq_argv[0], &fa, NULL, imq_argv, NULL);
+			posix_spawn_file_actions_destroy(&fa);
+			waitpid(pid, &status, 0);
+		}
+	#else
+		static void __attribute__((destructor))
+		_fault_metrics_link_counters(void)
+		{
+			_fault_update_telemetry();
+			symlink(_fault_metrics_link, _fault_lcounters);
 		}
 
-		// Merge the raw data so that it can be read.
-		posix_spawn_file_actions_init(&fa);
-		posix_spawn_file_actions_adddup2(&fa, STDERR_FILENO, STDOUT_FILENO);
-		posix_spawn(&pid, lpd_argv[0], &fa, NULL, lpd_argv, NULL);
-		posix_spawn_file_actions_destroy(&fa);
-		waitpid(pid, &status, 0);
-
-		// Prepare l-counters, and switch the metrics isolation.
-		fd = open(_fault_lcounters, O_WRONLY|O_CREAT|O_APPEND, S_IRUSR|S_IWUSR);
-		dprintf(fd, "&%s\n", _fault_metrics_isolation);
-
-		// Extract counters from the merged data.
-		posix_spawn_file_actions_init(&fa);
-		posix_spawn_file_actions_adddup2(&fa, fd, STDOUT_FILENO);
-		posix_spawn(&pid, imq_argv[0], &fa, NULL, imq_argv, NULL);
-		posix_spawn_file_actions_destroy(&fa);
-		waitpid(pid, &status, 0);
-	}
+		static void __attribute__((constructor))
+		_fault_metrics_link_chain(void)
+		{
+			static struct _fault_metrics_chain link;
+			link.next = _fault_metrics_link_stack;
+			link.update = _fault_metrics_link_counters;
+			_fault_metrics_link_stack = &link;
+		}
+	#endif
 
 	#if 0
 	static void __attribute__((constructor))
